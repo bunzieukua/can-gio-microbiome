33f7d1699d8015e0e72b931d11b8b3c15ead5359
Le Pham <pnhoangle@gmail.com>
2026-06-01 15:27:15 -0700

    Update 1_join_single_end_reads.sh

--- analysis/1_join_single_end_reads.sh
+++ analysis/1_join_single_end_reads.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 
+#===============================================#
 #Join demultiplexed fastq files with q2-vsearch
+#===============================================#
 
+
+#Set up folder and create new script file for this command
+touch Step1_Joinreads_vsearch.sh
+chmod +x Step1_Joinreads_vsearch.sh
+nano Step1_Joinreads_vsearch.sh
+
+#Open the script file and run the following command to join single-end reads
 qiime vsearch merge-pairs \
 --i-demultiplexed-seqs demux-paired-end.qz.qza \
 --o-merged-sequences joined-reads.qza \
 --o-unmerged-sequences unjoined-reads.qza \
 --p-threads 4
+
+#Verify paired reads quality
+qiime demux summarize \
+--i-data joined-reads.qza \
+--o-visualization vsearch_joinedreads_summary.qzv
+
+#To view the human-readable output
+qiime tools view vsearch_joinedreads_summary.qzv
